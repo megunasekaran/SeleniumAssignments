feb99a24b1e552879b370645d2eeb7f04f95b19c
megunasekaran <64186507+megunasekaran@users.noreply.github.com>
2022-05-04 16:39:20 +0530

    Update PrimeNumber.java

--- week1/day1/assignments/PrimeNumber.java
+++ week1/day1/assignments/PrimeNumber.java
@@ -6,29 +6,7 @@ public class PrimeNumber {
 
 	
 
-	/*
-
-	 * Goal: To find whether a number is a Prime number or not
-
-	 * 
-
-	 * input: 13
-
-	 * output: 13 is a Prime Number
-
-	 * 
-
-	 * Shortcuts:
-
-	 * 1) Print : type: syso, followed by: ctrl + space + enter
-
-	 * 2) To create a 'for' loop: type 'for', followed by ctrl + space + down arrow + enter
-
-	 * 3) To create an 'if' condition: type 'if', followed by ctrl + space +down arrow + enter
-
-	 *   
-
-	 * What are my learnings from this code?
+	
 
 	 * 1)
 
